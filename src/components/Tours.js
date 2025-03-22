278d273efeb14c1320dc08d572a7c34472cb2e2a
shambhaviraj25 <kashyapshambhavi1410@gmail.com>
2025-03-22 11:26:15 +0530

    initial-commit

---
+++ src/components/Tours.js
@@ -0,0 +1,23 @@
+import Card from './Card';
+
+function Tours({tours,removeTour}){
+    return(
+        <div className="container">
+            <div>
+                <h2 className='title'>Plan With Sam-Me</h2>
+            </div>
+
+            <div className="cards" >
+            {
+                tours.map( (tour) => {
+                    return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                })
+            }
+            </div>
+
+        </div>
+        
+    );
+}
+
+export default Tours;
